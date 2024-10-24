071c55c7886f7ba939e708e2c2235bf4447d864a
Anh Thu <21521490@gm.uit.edu.vn>
2024-10-24 23:37:10 +0700

    test

--- Backend/auth_service/APIs/signin_api.py
+++ Backend/auth_service/APIs/signin_api.py
@@ -1,17 +1,27 @@
+import os
+from dotenv import load_dotenv
 from fastapi import HTTPException
 from main import app
 from services import Account
 from JWT import Authentication
-import asyncpg  
+import asyncpg
 
-# Đăng nhập
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+env_path = os.path.join(current_dir, '..', '..', '..', 'Database', 'PostgreSQL', 'env')
+
+load_dotenv(dotenv_path=env_path)
+
 @app.post('/APIsignin')
 async def signin(request: Account):
-    async with asyncpg.create_pool(user='db_user', password='db_password',
-                                   database='db_name', host='localhost') as pool:
+    async with asyncpg.create_pool(
+        user=os.getenv("POSTGRES_USER_DEV"),
+        password=os.getenv("POSTGRES_PASSWORD_DEV"),
+        database=os.getenv("POSTGRES_DB"),
+        host=os.getenv("POSTGRES_HOST")
+    ) as pool:
         async with pool.acquire() as connection:
-            result = await request.checkAccount(connection)
-            if result:
+            if await request.checkAccount(connection):
                 token = Authentication().generate_token(request.username)
                 
                 info = await request.getInfoAccount(connection)
