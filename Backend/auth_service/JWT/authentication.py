427991bc80e842f4166005a86466775de9c08ce5
Anh Thu <21521490@gm.uit.edu.vn>
2024-10-26 11:54:55 +0700

    api signin

--- Backend/auth_service/JWT/authentication.py
+++ Backend/auth_service/JWT/authentication.py
@@ -11,8 +11,8 @@ reusable_oauth2 = HTTPBearer(scheme_name='Authorization')
 
 class Authentication:
     def __init__(self):
-        self.SECRET_KEY=os.environ["SECRET_KEY"]
-        self.SECURITY_ALGORITHM=os.environ["SECURITY_ALGORITHM"]
+        self.SECRET_KEY="71d242901cca21e4ac52adab1cc5e7322cbadf7e432991fffa331ba46d4f2de9892fb76c1ffd5b140b03ba44a0bee009c303d82fac35d15ffcb4a8950f303f1057a921e193b2352d7451979d397a5273b867489bb28f817e972ed8c90cc1e1630321548b02ff19902d1a248525c45ab3ffab0077cdf11fab59bb9d74327a97279474f2f43ddfc1884187b994fbe8e44e237c17f4d6ee7dcb3ab0e8976d6065e012a7addd8b06fb8dc388d2787f69142d55eef203850395262be16be09c3269b55610d6a52a72180b43f924e743972420af8a93b510e913d3dabc234ab75b3cef3d2c66bace05f1b52037b06c898a7b23d62fd2d74465a571fea5cb3c9b5f4572"
+        self.SECURITY_ALGORITHM="HS256"
 
     #create JWT    
     def generate_token(self,username: Union[str, Any]) -> str:
--- Backend/auth_service/services/signin_service.py
+++ Backend/auth_service/services/signin_service.py
@@ -1,75 +1,31 @@
-from pydantic import BaseModel
-import hashlib
+import os
+import psycopg2
+from dotenv import load_dotenv
+from schemas.account_schemas import Account
+from JWT import Authentication
+from fastapi import HTTPException
 
-def compute_hash(data: str) -> str:
-    hash_object = hashlib.sha256()
-    hash_object.update(data.encode('utf-8'))
-    return hash_object.hexdigest()
+env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', '..', 'Database', 'PostgreSQL', '.env')
+load_dotenv(dotenv_path=env_path)
 
-class Account(BaseModel):
-    username: str
-    password: str
-    email: str = None
-    OTP: str = None
+DB_CONFIG = {
+    "user": os.getenv("POSTGRES_USER_DEV"),
+    "password": os.getenv("POSTGRES_PASSWORD_DEV"),
+    "dbname": os.getenv("POSTGRES_DB"),
+    "host": os.getenv("POSTGRES_HOST"),
+    "port": os.getenv("POSTGRES_PORT")
+}
 
-    # async def insertAccount(self, connection):
-    #     hashed_password = compute_hash(self.password)
-    #     query = """
-    #         INSERT INTO account (username, email, password) 
-    #         VALUES ($1, $2, $3)
-    #     """
-    #     await connection.execute(query, self.username, self.email, hashed_password)
-
-    async def checkAccount(self, connection) -> bool:
-        query = """
-            SELECT password FROM account WHERE username = $1
-        """
-        account = await connection.fetchrow(query, self.username)
-        if account is None or account['password'] != compute_hash(self.password):
-            return False
-        return True
-
-    async def insertOTP(self, connection, OTP: str):
-        query = """
-            INSERT INTO otp (username, otp, email) 
-            VALUES ($1, $2, $3, $4)
-        """
-        await connection.execute(query, self.username, OTP, self.email)
-
-    async def verifyEmail(self, connection) -> bool:
-        try:
-            query = """
-                SELECT otp FROM otp WHERE username = $1
-            """
-            otp_record = await connection.fetchrow(query, self.username)
-            if otp_record and otp_record['otp'] == self.OTP:
-                delete_query = """
-                    DELETE FROM otp WHERE username = $1
-                """
-                await connection.execute(delete_query, self.username)
-                return True
-        except:
-            return False
-        return False
-
-
-    # async def existenceUsername(self, connection) -> bool:
-    #     query = """
-    #         SELECT 1 FROM account WHERE username = $1
-    #     """
-    #     result = await connection.fetchrow(query, self.username)
-    #     return result is not None
-
-    # async def existenceEmail(self, connection) -> bool:
-    #     query = """
-    #         SELECT 1 FROM account WHERE email = $1
-    #     """
-    #     result = await connection.fetchrow(query, self.email)
-    #     return result is not None
-
-    async def getInfoAccount(self, connection) -> dict:
-        query = """
-            SELECT id, email, username FROM account WHERE username = $1
-        """
-        info = await connection.fetchrow(query, self.username)
-        return dict(info) if info else {}
+def signin_service(account: Account):
+    try:
+        with psycopg2.connect(**DB_CONFIG) as conn, conn.cursor() as cursor:
+            if account.checkAccount(cursor):
+                token = Authentication().generate_token(account.username)
+                info = account.getInfoAccount(cursor)
+                info.pop('password', None)
+                
+                return {'info': info, 'token': token}
+            else:
+                raise HTTPException(status_code=404, detail="User not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e}")
