e01587ea5d74282316c2bc5f4d8dc75b54159656
chiecbungbaton <21522371@gm.uit.edu.vn>
2024-10-26 12:42:59 +0700

    Sign Up screen

--- Frontend/RoadVision-Classifier/src/pages/User/Auth/Auth.tsx
+++ Frontend/RoadVision-Classifier/src/pages/User/Auth/Auth.tsx
@@ -3,8 +3,7 @@ import AuthComponent from "../../../components/Auth/AuthComponent";
 export default function Auth() {
   return (
     <div className="w-full h-screen bg-[#CFEEFF] flex justify-center items-center">
-
-        <AuthComponent/>
+        <AuthComponent />
     </div>
-  )
+  );
 }
