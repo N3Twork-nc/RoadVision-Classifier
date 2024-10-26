f4072f4b5d4e273a1797da96d493a6849e187e61
thainhatthu <21522648@gm.uit.edu.vn>
2024-10-26 16:00:44 +0700

    call api not complete

--- Frontend/RoadVision-Classifier/src/components/Auth/Signup/SignupBlock.tsx
+++ Frontend/RoadVision-Classifier/src/components/Auth/Signup/SignupBlock.tsx
@@ -1,12 +1,50 @@
 import gg from "../../../assets/img/gg.png";
 import fb from "../../../assets/img/fb.png";
 import React from "react";
+import { z } from "zod";
+import { axiosRequest } from "../../../config/axios.config";
+import { useState } from "react";
 
 interface SignupBlockProps {
   handleAuth: () => void;
 }
 
+// đặt type cho formData
+const loginSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
+type FormData = z.infer<typeof loginSchema>;
+
 const SignupBlock: React.FC<SignupBlockProps> = ({ handleAuth }) => {
+  // thiết lập formData và Error
+  const [formData, setFormData] = useState<FormData>({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState<string | null>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSignupClick = async () => {
+    setError(null);
+    // Gọi API login
+    try {
+      const response = await axiosRequest.post("api/signup", formData);
+      console.log("Login successful:", response.data);
+    } catch (err) {
+      setError("Login failed. Please check your information!");
+      console.error(err);
+    }
+  };
+  
+
   return (
     <div className="p-4 sm:p-10 flex flex-col gap-2 items-center justify-center">
       <div className="Header w-full md:text-4xl text-3xl text-[#23038C] font-bold text-left">
@@ -59,7 +97,10 @@ const SignupBlock: React.FC<SignupBlockProps> = ({ handleAuth }) => {
         <label className="inline-flex items-center">
           Already have an account?{" "}
         </label>
-        <a onClick={handleAuth} className="cursor-pointer text-sm font-bold ml-1">
+        <a
+          onClick={handleAuth}
+          className="cursor-pointer text-sm font-bold ml-1"
+        >
           Login
         </a>
       </div>
@@ -83,7 +124,7 @@ const SignupBlock: React.FC<SignupBlockProps> = ({ handleAuth }) => {
         <button className="w-20 h-10 sm:w-15 sm:h-15 rounded-lg border-[2px] border-[#a5b3ff] flex justify-center items-center">
           <img src={gg} alt="Google" className="w-5 h-5 sm:w-6 sm:h-6" />
         </button>
-      </div>        
+      </div>
     </div>
   );
 };
