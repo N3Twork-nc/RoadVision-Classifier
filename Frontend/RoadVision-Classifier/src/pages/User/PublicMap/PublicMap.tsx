98a6c954130ca9a81b9acab36fe8675bda7d9d48
chiecbungbaton <21522371@gm.uit.edu.vn>
2024-11-17 16:41:57 +0700

    Done layout, Home

---
+++ Frontend/RoadVision-Classifier/src/pages/User/PublicMap/PublicMap.tsx
@@ -0,0 +1,5 @@
+export default function PublicMap() {
+  return (
+    <div className="w-full min-h-screen bg-[#F9F9F9] flex flex-col p-5 gap-5 justify-start items-center overflow-y-auto">PublicMap</div>
+  )
+}
--- Frontend/RoadVision-Classifier/src/routes/path.ts
+++ Frontend/RoadVision-Classifier/src/routes/path.ts
@@ -1,5 +1,8 @@
 export const PATH = {
     HOME: "/",
+    PUBLICMAP: "/public-map",
+    MAPMANAGEMENT: "/map-management",
+    LIBRARY: "/library",
     AUTH: "auth",
     FORGOT_PASSWORD: "forgot-password",
     PROFILE: "profile",
