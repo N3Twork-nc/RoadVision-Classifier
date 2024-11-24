37c08626c526eeac131dccfad987e70a379039cd
thinc <caothiu2003@gmail.com>
2024-11-24 11:45:13 +0700

    fix auth

--- Backend/auth_service/main.py
+++ Backend/auth_service/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-app = FastAPI()
+app = FastAPI( root_path="/auth")
 from APIs import *
 import dotenv
 app.add_middleware(
