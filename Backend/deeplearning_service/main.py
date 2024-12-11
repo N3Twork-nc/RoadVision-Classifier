415cf89f072932e957a403c6134830a42bc9dec9
thinc <caothiu2003@gmail.com>
2024-12-11 20:37:21 +0700

    structure folder of user service

--- Backend/deeplearning_service/main.py
+++ Backend/deeplearning_service/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from Crontab import classifier_image
 app = FastAPI()
 from APIs import *
 import dotenv
--- Backend/user_service/Database/__init__.py
+++ Backend/user_service/Database/__init__.py
@@ -1,2 +1,1 @@
-from .mongodb import *
 from .postgresql import *
