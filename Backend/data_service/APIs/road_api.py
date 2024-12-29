85de16224172b2bb2829a98c24d90f28c51dd5dc
thinc <caothiu2003@gmail.com>
2024-12-29 17:08:08 +0700

    [datasvc] feature getroute and edit road

--- Backend/data_service/APIs/road_api.py
+++ Backend/data_service/APIs/road_api.py
@@ -1,5 +1,5 @@
 from main import app
-from services import RoadService,validate_token
+from services import RoadService,validate_token,RouteMap
 from fastapi import File, UploadFile,Form, Depends
 from fastapi.responses import JSONResponse
 from schemas import RoadSchema
@@ -42,4 +42,17 @@ def get_roads(user_id: int=None, id_road: int=None):
         print(current_file_path, e)
         return JSONResponse(content={"status": "error", "message": "Internal server error"}, status_code=500)
 
-# @app.get("/api/getInfoRoad")
+@app.get("/api/getRouteMap")
+async def get_route_map():
+    try:
+        return RouteMap.get_route_map()
+    except Exception as e:
+        print(current_file_path, e)
+        return JSONResponse(content={"status": "error", "message": "Internal server error"}, status_code=500)
+@app.patch("/api/updateLocationRoad")
+async def update_locationRoad(id:int,latitude:float,longitude:float,username = Depends(validate_token)):
+    try:
+        return RoadService.updateLocationRoad(id,latitude,longitude,username)
+    except Exception as e:
+        print(current_file_path, e)
+        return JSONResponse(content={"status": "error", "message": "Internal server error"}, status_code=500)
