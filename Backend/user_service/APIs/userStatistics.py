de5dd43c2e7893f87c8aec8b6666738146a237d3
Anh Thu <21521490@gm.uit.edu.vn>
2025-01-01 23:26:26 +0700

    get valid ward for assign task

--- Backend/user_service/APIs/userStatistics.py
+++ Backend/user_service/APIs/userStatistics.py
@@ -1,5 +1,5 @@
 from main import app
-from fastapi import Depends
+from fastapi import Depends, Query
 from services.statistics_service import StatisticsService
 from services.auth_validate import validate_token
 
@@ -10,3 +10,7 @@ def get_user_statistics(user_data: dict = Depends(validate_token)):
 @app.get('/api/getTechnicalStatistics')
 def get_technical_statistics(user_data: dict = Depends(validate_token)):
     return StatisticsService.list_all_technicals(user_data)
+
+@app.get('/api/getValidWard')
+def get_valid_ward(user_data: dict = Depends(validate_token)):
+    return StatisticsService.get_valid_wards(user_data)
